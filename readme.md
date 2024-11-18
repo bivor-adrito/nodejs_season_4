8e351124835eaa5a545718e352235d7ce5211304
bivor-adrito <bivor500@gmail.com>
2024-11-18 21:58:52 +0600

    ✨ Product complete

--- readme.md
+++ readme.md
@@ -66,10 +66,10 @@ Overview
     - Admins will be able to do the followings
         1. Admin will be able to create a product [DONE]
         2. Admin will be able to upload an image on the product [DONE]
-        3. Admin will be able to update a product []
-        4. Admin will be able to see all the products []
-        5. Admin will be able to see a specific product []
-        6. Admin will be able to delete [amend] a product []
+        3. Admin will be able to update a product [DONE]
+        4. Admin will be able to see all the products [DONE]
+        5. Admin will be able to see a specific product [DONE]
+        6. Admin will be able to delete [amend] a product [DONE]
         7. Admin will be able to see a specific order []
         8. Admin will be able to make change to an order []
         9. Admin will be able to update status of a order []
@@ -77,17 +77,17 @@ Overview
 
 
     - Customers will be able to do the followings
-        1. Customers will be able to see all the products []
-        2. Customers will be able to see the details of a product []
+        1. Customers will be able to see all the products [DONE]
+        2. Customers will be able to see the details of a product [DONE]
         3. Customers will be able to make an order []
         4. Customers will be able to see a specific order []          
         5. Customers will be able to cancel an order []
 
     - Customers will NOT be able to do the followings
-        1. Customers will not be able to create a product []
-        2. Customers will not be able to upload an image on the product []
-        3. Customers will not be able to update a product []
-        4. Customers will not be able to delete a product []
+        1. Customers will not be able to create a product [DONE]
+        2. Customers will not be able to upload an image on the product [DONE]
+        3. Customers will not be able to update a product [DONE]
+        4. Customers will not be able to delete a product [DONE]
 
 Database Structure
  - User {
--- routes/api/products.js
+++ routes/api/products.js
@@ -155,4 +155,59 @@ router.get("/", authenticateToken, async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
+
+//? Get One Product
+router.get('/:id', authenticateToken, async(req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findById(id).populate(['userId', 'fileId']).exec();
+        if (product) {
+          return res.json(product);
+        } else {
+          return res.status(404).json({ message: "User not found" });
+        }
+      } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
+})
+
+//? Update One Product
+router.put('/:id',authenticateToken, async(req, res) => {
+    try {
+        if (req.user.userType != "admin") {
+            return res.status(401).json({ message: "You are not an admin" });
+          }else{
+            const id = req.params.id
+            const body = req.body
+            const updateProduct = await Product.findByIdAndUpdate(id, body, {new: true})
+            if(updateProduct){
+                return res.json(updateProduct)
+            }else{
+                return res.status(404).json({message: "Product not found"})
+            }
+          }
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });    
+    }
+})
+
+//? Amend a product 
+router.delete('/:id', authenticateToken, async(req, res) => {
+    try {
+        if (req.user.userType != "admin") {
+            return res.status(401).json({ message: "You are not an admin" });
+          }else{
+            const id = req.params.id
+            const amendedProduct = await Product.findByIdAndUpdate(id, {isDeleted: true}, {new: true})
+            if(amendedProduct){
+                return res.json(amendedProduct)
+            }else{
+                return res.status(404).json({message: "Product not found"})
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });    
+        
+    }
+})
 module.exports = router;
